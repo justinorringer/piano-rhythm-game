399be7697c07233a9e64f9cf35a550217d36851f
justinorringer <justinorringer@gmail.com>
2025-03-12 01:43:34 -0400

    Generating "scale"

---
+++ example/main.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var csound: CsoundGodot
+
+@onready var csd = 'res://piano.csd'
+
+func _ready():
+	CsoundServer.connect("csound_layout_changed", csound_layout_changed)
+
+
+func csound_layout_changed():
+	csound = CsoundServer.get_csound("Main")
+	csound.send_control_channel("cutoff", 1)
+
+	csound.csound_ready.connect(csound_ready)
+
+
+func csound_ready(csound_name):
+	if csound_name != "Main":
+		return
+
+
+func _process(_delta):
+	pass
+
+
+func _generate(_toggled_on: bool) -> void:
+	csound.compile_csd(FileAccess.get_file_as_string(csd))
+	var notes = [261, 277, 293, 311, 329, 349, 369, 392, 415, 440]
+	for i in range(10):
+		# an arg is wrong but you get the idea
+		# first arg is seconds from the current time.
+		var event = 'i "piano" %d 1 %d 0.09 %d' % [i, i, notes[i]]
+		csound.event_string(event)
+
+	await get_tree().create_timer(10.0).timeout
