f9b0c7cf57f917e1c196e69b38294d11d334c7b8
justinorringer <justinorringer@gmail.com>
2025-03-12 10:53:46 -0400

    Attempt to make piano play notes

--- Entities/Piano/piano_key.gd
+++ Entities/Piano/piano_key.gd
@@ -5,6 +5,64 @@ extends Node3D
 @onready var anim_player = $AnimationPlayer
 @onready var mesh = $MeshInstance3D
 
+var _csound_ready = false
+var csound: CsoundGodot
+
+func _ready():
+	CsoundServer.connect("csound_layout_changed", csound_layout_changed)
+
+
+func csound_layout_changed():
+	csound = CsoundServer.get_csound("Main")
+	csound.send_control_channel("cutoff", 1)
+
+	csound.csound_ready.connect(csound_ready)
+
+
+func csound_ready(csound_name):
+	if csound_name != "Main":
+		return
+	_csound_ready = true
+	csound.compile_csd("""
+<CsoundSynthesizer>
+<CsInstruments>
+
+instr buzz_instrument
+
+iChan = p4
+iFreq mtof p5
+iAmp = p6 / 127
+iFn  = 1
+
+kSeg linsegr iAmp, .5, iAmp / 2, 1, 0
+
+asig buzz 1, iFreq, kSeg * iAmp, iFn
+outs asig, asig
+
+endin
+
+instr buzz_instrument2
+
+iChan = p4
+SNote = p5
+iFreq = ntof:i(SNote)
+iAmp = p6 / 127
+iFn  = 1
+
+kSeg linsegr iAmp, .5, iAmp / 2, 1, 0
+
+asig buzz 1, iFreq, kSeg * iAmp, iFn
+outs asig, asig
+
+endin
+
+</CsInstruments>
+<CsScore>
+f 1 0 16384 10 1
+</CsScore>
+</CsoundSynthesizer>
+""")
+
 enum KeyState { OFF, ON }
 var state = KeyState.OFF
 
@@ -24,9 +82,16 @@ func set_state(new_state: KeyState):
 				anim_player.play("on")
 
 func _input(event):
+	if not _csound_ready:
+		return
+		
 	if event is InputEventKey and key_index in key_map:
 		if event.keycode == key_map[key_index]:
 			if event.pressed:
+				csound.note_on(2, 67, 90)
+				#csound.note_on(key_index, key_index + 100, 90)
 				set_state(KeyState.ON)
 			else:
+				csound.note_off(2, 67)
+				#csound.note_off(key_index, key_index + 100)
 				set_state(KeyState.OFF)
