27b2ce292a3ba460d210f2bf4f076ca14deb8cd0
CodyWMitchell <codymitchelldev@gmail.com>
2025-03-17 16:30:12 -0400

    create score counter

--- globals/GameState.gd
+++ globals/GameState.gd
@@ -2,7 +2,19 @@ extends Node
 
 @export var notes = []
 @export var tempo = 120
+@export var score = 0
+@export var max_combo = 0
+@export var current_combo = 0
 
 func store(gen_notes, gen_tempo):
 	notes = gen_notes
 	tempo = gen_tempo
+
+func hit_note():
+	score += current_combo
+	current_combo += 1
+	if (max_combo < current_combo):
+		max_combo = current_combo
+
+func miss_note():
+	current_combo = 0
