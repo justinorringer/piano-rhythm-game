ae8c1d62b73b6d9fe11516db8044b11fc657b48f
justinorringer <justinorringer@gmail.com>
2025-03-17 16:05:02 -0400

    Recording melody separately

--- globals/GameState.gd
+++ globals/GameState.gd
@@ -1,8 +1,10 @@
 extends Node
 
+@export var melody = [] # for playing along
 @export var notes = []
 @export var tempo = 120
 
-func store(gen_notes, gen_tempo):
+func store(gen_melody, gen_notes, gen_tempo):
+	melody = gen_melody
 	notes = gen_notes
 	tempo = gen_tempo
