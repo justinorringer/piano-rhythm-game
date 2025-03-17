2b370bd7ec4f86ff69b4a29ee7a7ec480a23a233
justinorringer <justinorringer@gmail.com>
2025-03-17 15:25:14 -0400

    Scene triggers player

--- globals/GameState.gd
+++ globals/GameState.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var notes = []
-@export var tempo = []
+@export var tempo = 120
 
 func store(gen_notes, gen_tempo):
 	notes = gen_notes
--- screens/game.gd
+++ screens/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	Player.play(GameState.notes, GameState.tempo)
 
 func _on_button_pressed() -> void:
 	SceneSwitch.switch_scene("res://screens/score.tscn")
